473fb964eabe03587c03c5d8792c22c28e9b2ff9
Sally Resch <sallyresch27@gmail.com>
2022-07-04 13:26:22 +0200

    Initial commit

---
+++ src/udemy/javamasterclass/Main.java
@@ -0,0 +1,11 @@
+package udemy.javamasterclass;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double poundsToConvert = 5d;
+    double poundsToKilograms= poundsToConvert * 0.45359237d;
+
+        System.out.println("Five pounds is equal to:" + poundsToKilograms +" kilograms");
+    }
+}
